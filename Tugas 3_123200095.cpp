f280947ed21278bdea1201540186a3921f1fb389
nailifd <73066418+nailifd@users.noreply.github.com>
2020-11-01 23:58:37 +0700

    Add files via upload

---
+++ Tugas 3_123200095.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+main (void) {
+    int username, password=12345, naili, pass;
+   int i;
+cout << "------------------------------------------\n";
+cout << "Login\n";
+cout << "------------------------------------------\n";
+  ;int user = naili;
+  ;pass = 12345;
+  i = 1;
+   do {
+       cout << "Username : "; cin >> username;
+      cout << "Password : "; cin >> password;
+
+       if (username = user && password == pass) {
+         cout << "\n--------------------\n";
+          cout << "Anda Berhasil Login" << endl;
+         cout << "--------------------\n\n";
+      } else {
+          cout << "\n------------------------\n";
+          cout << "Maaf ID & PASSWORD Salah!" << endl;
+         cout << "-------------------------\n\n";
+         i = i +1;
+      }
+   ;}  
+
